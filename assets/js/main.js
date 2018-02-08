75b4ff2a29adc7f114913febd4a6c58ad5e77cde
keshav99 <kesh.howzat@gmail.com>
2018-02-08 09:31:33 +0530

    Landing page and hamburger added

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,7 @@ function init() {
         $.fn.pagepiling.moveSectionDown();
         setTimeout(deleteNode,200);
         $('.navbar').css("display","block");
-    
+
     });
 }
 
@@ -50,3 +50,79 @@ function deleteNode() {
     $('#intro-section').css("display","none");
     $.fn.pagepiling.moveSectionUp();
 }
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
+
+var x=0;
+$(document).ready(function(){
+	$('#nav-icon3').click(function(){
+		$(this).toggleClass('open');
+    x++;
+    if(x%2!=0){
+      $('.extend-right').css('transform','translateX(90vw)');
+    }
+    else {
+      $('.extend-right').css('transform','translateX(100vw)');
+    }
+
+	});
+
+  $("#bottom-arrow").click(function(){
+    $.fn.pagepiling.moveSectionDown();
+  });
+});
